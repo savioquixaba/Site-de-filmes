006688ba73466acf9b1095a39ccaf8dc772f935f
Savio Quixaba <76010162+savioquixaba@users.noreply.github.com>
2022-03-22 21:44:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+//TMDB 
+API_KEY = 'api_key=ca0f047481024882533aec3c32a2525b';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const main = document.getElementById("main");
+
+
+
+getMovies(API_URL);
+
+function getMovies(url){
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data){
+    main.innerHTML = 'a' ;
+    
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement("div");
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src="${IMG_URL+poster_path}" alt="${title}">
+                
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+    
+
+        <div class="overview">
+
+            <h3>Overview</h3>
+            ${overview}
+        </div>
+        
+        
+        `
+
+        main.appendChild(movieEl);
+    })
+}
+
+function getColor(vote){
+    if(vote>=8){
+        return "green"
+    }else if(vote>=5){
+        return "orange"
+    }else
+        return "red"
+}
